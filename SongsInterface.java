e2a34ff76f02e7baa40ea1a6f7a5c213703effc3
vinimanoj123 <128217172+vinimanoj123@users.noreply.github.com>
2023-04-14 18:29:01 +0530

    Update SongsInterface.java

--- SongsInterface.java
+++ SongsInterface.java
@@ -1,10 +1,11 @@
 package MobileUtility;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public interface SongsInterface {
 
     public abstract void addSong(String song, String user);
 
-    public abstract LinkedList<String> getRecentSongs(String user);
+    public abstract List<String> getRecentSongs(String user);
 }
