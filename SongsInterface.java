6501353fcc10412d1800620b6547a6a0d9730da4
vinimanoj123 <128217172+vinimanoj123@users.noreply.github.com>
2023-04-14 18:31:14 +0530

    Update SongsInterface.java

--- SongsInterface.java
+++ SongsInterface.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public interface SongsInterface {
 
-    public abstract void addSong(String song, String user);
+    public abstract void addSong(String user, String song);
 
     public abstract List<String> getRecentSongs(String user);
 }
